da2c0af0ab5801b69a25c08f3c91ab7a2a804284
harbor <hluo@bdnacorp.com>
2016-08-14 21:07:00 +0800

    add debug information

--- app/src/main/java/com/harbor/calculator/MainActivity.java
+++ app/src/main/java/com/harbor/calculator/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends Activity implements android.view.View.OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("onCreate", "Android calculator started.");
+        Log.d("Calculator", "onCreate() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
